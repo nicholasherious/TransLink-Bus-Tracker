5f2d22ed714dd1c096e1ca9c8c778d36d5ab66df
nicholasherious <nherious@gmail.com>
2020-04-15 23:07:42 -0700

    updated scheduled alert

--- src/components/BusAlert.js
+++ src/components/BusAlert.js
@@ -4,8 +4,8 @@ import { Alert, AlertTitle } from '@material-ui/lab';
 function Error() {
   return (
     <div>
-      <Alert severity="warning">
-        <AlertTitle>Alert</AlertTitle>
+      <Alert severity="info">
+        <AlertTitle>Information</AlertTitle>
         No buses are scheduled for this stop — Please try again later.
       </Alert>
     </div>
